9c2caaba0f33b5a9104bf04876b7d223e03d8b44
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-10-10 14:48:18 -0500

    folders

---
+++ adminPro/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NoPagesFoundComponent } from './pages/no-pages-found/no-pages-found.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NoPagesFoundComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
