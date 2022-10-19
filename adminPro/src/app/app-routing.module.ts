6ba46b41e54ca9e372ab176afb81a6de2ef09440
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-10-19 17:32:02 -0500

    router

--- adminPro/src/app/app-routing.module.ts
+++ adminPro/src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graph1Component } from './pages/graph1/graph1.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { NoPagesFoundComponent } from './pages/no-pages-found/no-pages-found.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    {
+      path:'dashboard', component: DashboardComponent
+    },
+    {
+      path:'login', component: LoginComponent
+    },
+    {
+      path:'register', component: RegisterComponent
+    },
+    {
+      path:'graphic1', component: Graph1Component
+    },
+    {
+      path:'progress', component: ProgressComponent
+    },
+    {
+      path:'', redirectTo: '/dashboard', pathMatch: 'full'
+    },
+    {
+      path:'**', component: NoPagesFoundComponent
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
