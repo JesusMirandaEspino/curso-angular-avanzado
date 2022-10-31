6742971e165731885bc9abf22c1f326c0ee358a5
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-10-31 11:13:35 -0600

    chart js part 2

--- adminPro/src/app/pages/graph1/graph1.component.ts
+++ adminPro/src/app/pages/graph1/graph1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartEvent, ChartType, Color } from 'chart.js';
 
 @Component({
   selector: 'app-graph1',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Graph1Component implements OnInit {
 
+// Doughnut
+  public doughnutChartLabels: string[] = [ 'Download Sales'];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [ 350, 450, 100 ],
+      backgroundColor: ['#00821C','#09DB36','#024D0F'],
+      hoverBackgroundColor: ['#00821C','#09DB36','#024D0F'],
+      hoverBorderColor:['#000000','#000000','#00000003']
+      },
+
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // events
+  public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
   }
 
 }
