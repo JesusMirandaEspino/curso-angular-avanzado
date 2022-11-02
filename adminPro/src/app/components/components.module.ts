906e68c52414b34c3fb5704967f24b51505bd8a3
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-11-02 09:10:49 -0600

    dona component

--- adminPro/src/app/components/components.module.ts
+++ adminPro/src/app/components/components.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IncrementComponent } from './increment/increment.component';
 import { FormsModule } from '@angular/forms';
+import { DonaComponent } from './dona/dona.component';
 
 
 @NgModule({
   declarations: [
-    IncrementComponent
+    IncrementComponent,
+    DonaComponent
   ],
   imports: [
     CommonModule,
