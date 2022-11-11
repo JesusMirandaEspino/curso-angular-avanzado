56962ec0fd5223f67b8159d10d38deac328c7edc
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-11-11 11:00:50 -0600

    services settings routerlink

--- adminPro/src/app/shared/shared.module.ts
+++ adminPro/src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { BreadCrumbsComponent } from './bread-crumbs/bread-crumbs.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -13,7 +14,8 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     BreadCrumbsComponent,
