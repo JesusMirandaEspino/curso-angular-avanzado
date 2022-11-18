3c70f16e754ced557babb4dcbba23051f542ba4c
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-11-18 08:23:55 -0600

    observer

--- adminPro/src/app/pages/rxjs/rxjs.component.ts
+++ adminPro/src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RxjsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    const obs$ = new Observable( observer => {
+      let i = 0;
+      const intervalor = setInterval( () => {
+        i++;
+        observer.next(i);
+
+        if( i === 4 ){
+          clearInterval(intervalor);
+          observer.complete();
+        }
+
+        console.log('tick');
+      }, 1000);
+    });
+
+    obs$.subscribe( val => console.log(val),
+                    error => console.log(error),
+                    () => console.log('terminado'));
+  }
 
   ngOnInit(): void {
+    // code
   }
 
 }
