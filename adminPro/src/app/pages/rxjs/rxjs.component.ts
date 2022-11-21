53604d0e88937aa7c768788b3025d60c0aa038ee
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-11-21 08:13:28 -0600

    interval

--- adminPro/src/app/pages/rxjs/rxjs.component.ts
+++ adminPro/src/app/pages/rxjs/rxjs.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import {  retry } from 'rxjs/operators';
+import { Observable, interval } from 'rxjs';
+import {  filter, map, retry, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -17,38 +17,47 @@ export class RxjsComponent implements OnInit {
         retry(1)
       ).subscribe( val => console.log(val),
                       error => console.log(error),
-                      () => console.log('terminado'));
+                      () => console.log('terminado')
+      );
+
+
+    this.returnInterval().subscribe( resp => console.log(resp),)
+
   }
 
   ngOnInit(): void {
     // code
   }
 
+  returnInterval():Observable<number>{
+    return  interval(1000).pipe(
+      map( valor => valor + 1),
+      filter(valor => (valor % 2 === 0) ? true : false),
+      take(10)
+    );
+  }
 
   returnObserver():Observable<number>{
     let i = 0;
-    const obs$ = new Observable<number>( observer => {
+    return new Observable<number>( observer => {
 
       const intervalor = setInterval( () => {
         i++;
         observer.next(i);
 
-        if( i === 4 ){
-          clearInterval(intervalor);
-          observer.complete();
-        }
+          if( i === 4 ){
+            clearInterval(intervalor);
+            observer.complete();
+          }
 
-        if( i === 2 ){
-          i = 0;
-          observer.error('error');
-        }
+          if( i === 2 ){
+            i = 0;
+            observer.error('error');
+          }
 
         console.log('tick');
       }, 1000);
     });
-
-
-    return obs$;
 
   }
 
