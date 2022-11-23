3f1d67e3bc9b023153eb7a4b33dd9a9c3c0f3588
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-11-23 08:56:11 -0600

    titulo parte 2

--- adminPro/src/app/shared/bread-crumbs/bread-crumbs.component.ts
+++ adminPro/src/app/shared/bread-crumbs/bread-crumbs.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
 @Component({
@@ -7,24 +8,33 @@ import { filter, map } from 'rxjs/operators';
   templateUrl: './bread-crumbs.component.html',
   styleUrls: ['./bread-crumbs.component.scss']
 })
-export class BreadCrumbsComponent implements OnInit {
+export class BreadCrumbsComponent implements OnInit, OnDestroy {
 
   public titulo: string = '';
+  tituloSub$: Subscription;
 
   constructor( private router: Router) {
-    this.router.events
-    .pipe(
-      filter((event): event is ActivationEnd => event instanceof ActivationEnd),
-      filter((event:ActivationEnd) => event.snapshot.firstChild === null ),
-      map((event:ActivationEnd) => event.snapshot.data)
-    ).subscribe( ({titulo}) => {
+    this.tituloSub$ =  this.getDataRoute().subscribe( ({titulo}) => {
       this.titulo = titulo;
       document.title = titulo;
-    });
+    });;
   }
 
   ngOnInit(): void {
     // code
+  }
+
+  ngOnDestroy(): void{
+    this.tituloSub$.unsubscribe();
+  }
+
+  getDataRoute(){
+    return this.router.events
+    .pipe(
+      filter((event): event is ActivationEnd => event instanceof ActivationEnd),
+      filter((event:ActivationEnd) => event.snapshot.firstChild === null ),
+      map((event:ActivationEnd) => event.snapshot.data)
+    )
   }
 
 }
