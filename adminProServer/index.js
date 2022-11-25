a4dccb035a3d1e8cacfe1d5f5a2f5a91953e310c
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-11-25 08:17:54 -0600

    rutas

--- adminProServer/index.js
+++ adminProServer/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 
 
+
+// routes
+app.get( '/', (req, res) => {
+    res.json({
+        ok: true,
+        msg: 'Hola mundo'
+    });
+});
+
 app.listen( 3000, () => {
     console.log('Servidor corriendo en el puerto 3000');
 });
