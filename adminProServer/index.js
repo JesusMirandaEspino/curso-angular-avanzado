50f71aa33debbeb7f4557e8058d5999c45125da2
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-11-30 09:50:26 -0600

    cors

--- adminProServer/index.js
+++ adminProServer/index.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const { conectionDB } = require('./db/config');
 require('dotenv').config();
+const { conectionDB } = require('./db/config');
+const express = require('express');
+const cors = require('cors');
+
+
 const app = express();
 
+// DB Conection
 conectionDB();
 
-// mean_user:uuw78DSfTMG4wErT
-// routes
+
+app.use(cors())
+
+
+// TODO routes
 app.get( '/', (req, res) => {
     res.status(200).json({
         ok: true,
@@ -14,6 +21,7 @@ app.get( '/', (req, res) => {
     });
 });
 
+// Server
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
 });
