08da9ff6d0b81b51f1dd8e56e593d5c8cb7aef6d
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-12-12 08:38:04 -0600

    jwt validar

--- adminProServer/routes/users.js
+++ adminProServer/routes/users.js
@@ -3,11 +3,12 @@ const { getUsers, createUser, updateUser, deleteUser } = require('../controller/
 const router = express.Router();
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
+const { validateJWT } = require('../middlewares/validar-jwt');
 
 
 router.get( '/', getUsers );
 
-router.post( '/', [
+router.post( '/', validateJWT,  [
     check('name', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password es requerido').not().isEmpty(),
     check('email', 'El email es obligatorio').isEmail(),
