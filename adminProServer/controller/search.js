c3f84cd248844be8712eff01f7eecb9e83748e65
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-12-21 08:22:32 -0600

    search table part 2

--- adminProServer/controller/search.js
+++ adminProServer/controller/search.js
@@ -7,7 +7,7 @@ const Medico = require('../models/medico.js');
 
 const getTodo = async (req, res = response) => {
 
-    const search = req.query.search;
+    const search = req.params.search;
     const regex = new RegExp(search, 'i');
 
     const [ user, medico, hospital ] = await Promise.all([ 
@@ -41,33 +41,42 @@ const getTodo = async (req, res = response) => {
 
 const getColection = async (req, res = response) => {
 
-    const search = req.query.search;
+    const table = req.params.table;
+    const search = req.params.search;
     const regex = new RegExp(search, 'i');
 
-    const [ user, medico, hospital ] = await Promise.all([ 
-            User.find({ name: regex }),
-            Medico.find({ name: regex }),
-            Hospital.find({ name: regex }), 
-    ])
+    let data;
 
-    try{
+    switch(table){
+
+        case 'medicos':
+                data = await Medico.find({ name: regex })
+                                        .populate( 'user', 'name img' )
+                                        .populate( 'hospital', 'name img' );
+                break;
+
+        case 'hospitals':
+                data = await Hospital.find({ name: regex }).populate( 'user', 'name img' );;
+                break;
+
+        case 'users':
+                data = await User.find({ name: regex });
+                break;
+
+        default: 
+            return res.status(400).json({
+            ok: false,
+            msg: 'Tiene que elegir una tabla'
+        });
+    }
 
         res.status(200).json({
             ok: true,
             msg: 'Hola mundo',
-            search,
-            user,
-            medico,
-            hospital
+            respond: data
+
         });
 
-    }catch(e){
-        console.log(err);
-        res.status(500).json({
-            ok: false,
-            msg: 'Error inesperado'
-        }); 
-    }
 }
 
 module.exports = {
